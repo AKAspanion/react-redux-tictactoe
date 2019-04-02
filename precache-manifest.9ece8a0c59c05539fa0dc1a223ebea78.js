0d399a3bb7335582d0814c063ca4ff0544ae5cb9
AKAspanion <panditankit1995@gmail.com>
2019-04-02 21:14:37 +0530

    Updates

---
+++ precache-manifest.9ece8a0c59c05539fa0dc1a223ebea78.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9465613f7f111f2af45f",
+    "url": "/react-redux-tictactoe/static/js/runtime~main.813fc6c6.js"
+  },
+  {
+    "revision": "ec0243c5724ceac1524e",
+    "url": "/react-redux-tictactoe/static/js/main.7d162351.chunk.js"
+  },
+  {
+    "revision": "5fc613c792b42ecc24e0",
+    "url": "/react-redux-tictactoe/static/js/2.892de40e.chunk.js"
+  },
+  {
+    "revision": "ec0243c5724ceac1524e",
+    "url": "/react-redux-tictactoe/static/css/main.731888ed.chunk.css"
+  },
+  {
+    "revision": "aeff2dc7e9f46735749edbfbf78bc127",
+    "url": "/react-redux-tictactoe/index.html"
+  }
+];
